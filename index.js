1ce7967ba35d9f7e7865089b7981efab31bf61f4
gabvilla <gabriel.villassanti2@hotmail.com>
2024-08-21 22:22:49 -0300

    implemented the validation logic and started the array numeral logic

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const userNumber = document.querySelector('#number')
 const convertButton = document.querySelector('#convert-btn');
 const output = document.querySelector('#output');
+const romanNumerals = {
+   'M': 1000,
+   'CM': 900,
+   'D': 500,
+   'CD': 400,
+   'C': 100,
+   'XC': 90,
+   'L': 50,
+   'XL': 40,
+   'X': 10,
+   'IX': 9,
+   'V': 5,
+   'IV': 4,
+   'I': 1
+}
 
 convertButton.addEventListener(("click"), () => {
-   if (userNumber.value == ""){
+   if (userNumber.value == "" || isNaN(userNumber.value)){
       output.innerHTML = ('Please enter a valid number');
    }else if (userNumber.value <= 0) {
       output.innerHTML = ('Please enter a number greater than or equal to 1');
    } else if (userNumber.value > 3999){
       output.innerHTML = ('Please enter a number less than or equal to 3999');
+   } else {
+      let numberArray = []
+      let arraySize;
+      numberArray = Array.from(userNumber.value);
+      arraySize = numberArray.length;
+      console.log(romanNumerals.C * 3); 
    }
 })
