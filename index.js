24bbc8029ed55e7bfafa87cb7f31aca3c38c9fbb
gabvilla <gabriel.villassanti2@hotmail.com>
2024-08-25 12:25:07 -0300

    were inserted the array methods to start the converter main logical

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const userNumber = document.querySelector('#number')
 const convertButton = document.querySelector('#convert-btn');
 const output = document.querySelector('#output');
-const romanNumerals = {
-   'M': 1000,
-   'CM': 900,
-   'D': 500,
-   'CD': 400,
-   'C': 100,
-   'XC': 90,
-   'L': 50,
-   'XL': 40,
-   'X': 10,
-   'IX': 9,
-   'V': 5,
-   'IV': 4,
-   'I': 1
-}
+const romanNumerals = [
+   ['M', '1000'],
+   ['CM', '900'],
+   ['D', '500'],
+   ['CD', '400'],
+   ['C', '100'],
+   ['XC', '90'],
+   ['L', '50'],
+   ['XL', '40'],
+   ['X', '10'],
+   ['IX', '9'],
+   ['V', '5'],
+   ['IV', '4'],
+   ['I', '1']
+]
 
 convertButton.addEventListener(("click"), () => {
+   let resultArray = []
    if (userNumber.value == "" || isNaN(userNumber.value)){
       output.innerHTML = ('Please enter a valid number');
    }else if (userNumber.value <= 0) {
@@ -25,10 +26,14 @@ convertButton.addEventListener(("click"), () => {
    } else if (userNumber.value > 3999){
       output.innerHTML = ('Please enter a number less than or equal to 3999');
    } else {
-      let numberArray = []
-      let arraySize;
-      numberArray = Array.from(userNumber.value);
-      arraySize = numberArray.length;
-      console.log(romanNumerals.C * 3); 
+      let inputArray = []
+      inputArray = Array.from(userNumber.value);
+      romanNumerals.forEach(element => {
+        let numberArray = Array.from(romanNumerals[romanNumerals.indexOf(element)][1])
+        if (inputArray.length == numberArray.length){
+         resultArray.push(romanNumerals[romanNumerals.indexOf(element)][0])
+         console.log(resultArray);
+        }
+      });
    }
 })
