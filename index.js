be6c87e9633fb37fc1ca2c724f12019294d74edb
gabvilla <gabriel.villassanti2@hotmail.com>
2024-08-26 23:02:46 -0300

    created separated functions for form validation and output clear and finished the converter main logical

--- index.js
+++ index.js
@@ -1,39 +1,77 @@
-const userNumber = document.querySelector('#number')
+const form = document.querySelector('form')
 const convertButton = document.querySelector('#convert-btn');
 const output = document.querySelector('#output');
-const romanNumerals = [
-   ['M', '1000'],
-   ['CM', '900'],
-   ['D', '500'],
-   ['CD', '400'],
-   ['C', '100'],
-   ['XC', '90'],
-   ['L', '50'],
-   ['XL', '40'],
-   ['X', '10'],
-   ['IX', '9'],
-   ['V', '5'],
-   ['IV', '4'],
-   ['I', '1']
-]
 
-convertButton.addEventListener(("click"), () => {
-   let resultArray = []
+const convertToRoman = num => {
+   const romanNumerals = [
+      ['M', 1000],
+      ['CM', 900],
+      ['D', 500],
+      ['CD', 400],
+      ['C', 100],
+      ['XC', 90],
+      ['L', 50],
+      ['XL', 40],
+      ['X', 10],
+      ['IX', 9],
+      ['V', 5],
+      ['IV', 4],
+      ['I', 1]
+   ];
+
+   const res = [];
+
+   romanNumerals.forEach(function (arr) {
+      while (num >= arr[1]) {
+         res.push(arr[0]);
+         num -= arr[1];
+       }         
+   });
+
+   return res.join('');
+};
+
+const formValidation = () => {
+   const errorText = '';
+
    if (userNumber.value == "" || isNaN(userNumber.value)){
-      output.innerHTML = ('Please enter a valid number');
+      errorText = 'Please enter a valid number';
    }else if (userNumber.value <= 0) {
-      output.innerHTML = ('Please enter a number greater than or equal to 1');
+      errorText = 'Please enter a number greater than or equal to 1';
    } else if (userNumber.value > 3999){
-      output.innerHTML = ('Please enter a number less than or equal to 3999');
+      errorText = 'Please enter a number less than or equal to 3999';
    } else {
-      let inputArray = []
-      inputArray = Array.from(userNumber.value);
-      romanNumerals.forEach(element => {
-        let numberArray = Array.from(romanNumerals[romanNumerals.indexOf(element)][1])
-        if (inputArray.length == numberArray.length){
-         resultArray.push(romanNumerals[romanNumerals.indexOf(element)][0])
-         console.log(resultArray);
-        }
-      });
+      return true
+   };
+
+   output.innerText = errorText;
+
+   return false
+};
+
+const clearOutput = () => {
+   output.innerText = '';
+}
+
+form.addEventListener('submit', e => {
+   e.preventDefault();
+   updateUI();
+ });
+
+ convertButton.addEventListener('click', () => {
+   updateUI();
+ })
+
+ const updateUI = () => {
+   const numStr = document.querySelector('#number').value;
+   const int = parseInt(numStr, 10);
+
+   clearOutput();
+
+   if (formValidation){
+      output.innerText = convertToRoman(int);
    }
-})
+ }
+
+
+
